6491d5401bca549082f1cba572037854d1a53f45
victormanoly <victor.manoly@gmail.com>
2018-11-26 12:03:49 -0200

    tested with web server

--- untag.py
+++ untag.py
@@ -1,18 +1,19 @@
-#probando uso de sockets para modificar paquetes
 from struct import *
 from socket import *
 import binascii, optparse
 #from scapy.all import *
 
-
-class Tag():
+class UnTag():
 
     def __init__(self, *args, **kwargs):
         self.iface = "enp0s3"
+        self.tag_list = []
         self.s = socket(AF_PACKET, SOCK_RAW, ntohs(0x0003))
+        self.protoip = ((hex(2048)).replace("0x", "0")).decode('hex')
         self.s.bind((self.iface,0))
-        self.tag_count = 1501
         self.rx_pkts()
+
+
 
     #Receive packets in the interface
     def rx_pkts(self):
@@ -20,31 +21,23 @@ class Tag():
             print ("Receive Packets")
             self.pkts = self.s.recvfrom(65565)
             print (self.pkts)
-            if self.pkts[1][1] == 2048 and self.pkts[1][2] == 0: #ip type and destinated to the host
-                self.parse_pkts(self.pkts[0])
-            else:
-                print ("Send Packet")
-                self.s.send(self.pkts[0])  
+            self.parse_pkts(self.pkts[0])
+            
 
-    #Create Tag
-    def tag(self):
-        print("Tagging Packets")
-        if self.tag_count == 1532:
-            self.tag_count = 1501
-        tag = (hex(self.tag_count)).replace("0x", "0")
-        self.n_protot = tag.decode("hex")
-        self.tag_count += 1
-        self.tx_pkts()
-
-    
-    #Send tagged packets
+    #Send untagged packets
     def tx_pkts(self):
-        print ("Send Tagged Packet")
-        self.tag_pkt = self.dmac + self.smac + self.n_protot + self.payload
-        self.s.send(self.tag_pkt)
-           
+        print ("Send UnTagged Packet")
+        self.untag_pkt = self.dmac + self.smac + self.protoip + self.payload
+        self.s.send(self.untag_pkt)
 
-
+    def untag(self):
+        print("untagging")
+        if self.protot not in self.tag_list: 
+            self.tag_list.append(self.protot)
+            if self.protot == 1531:
+                self.tag_list = []
+            self.tx_pkts()
+        self.rx_pkts()
 
     def parse_pkts(self, pkt):
         print ("Parsing Packets")
@@ -60,22 +53,19 @@ class Tag():
 
         payload = binascii.hexlify(pkt[14:]).decode()
         self.payload = payload.decode("hex")
+        
 
-        self.tag()
+        if int(protot,16) > 1500 and int(protot,16) <= 1531: # and self.pkts[1][2] == 0: #ip type and destinated $
+            self.untag()
+        #else:
+        #    print ("Send Packet")
+        #    self.s.send(self.pkts[0])
+
+
 
 
 def __main__():
-    pd = Tag()
+    pd = UnTag()
 
 if __name__ == '__main__':
     __main__()
-
-
-
-
-
-
-    
-
-
-
